67e3b1429cf36263ddb7ca822d687770d554168f
yehani <yehanipamunuwa@gmail.com>
2024-05-03 08:11:02 +0530

    adding index.html for spa

---
+++ assets/js/dashboard.js
@@ -0,0 +1,176 @@
+/*
+
+$('#dashboard-section').css({
+    display:'block'
+});
+
+$('#customer-section').css({
+    display:'none'
+});
+
+$('#item-section').css({
+    display:'none'
+});
+
+$('#order-section').css({
+    display:'none'
+});
+
+$('#nav-dashboard').on('click',() =>{
+    $('#dashboard-section').css({
+        display:'block'
+    });
+
+    $('#customer-section').css({
+        display:'none'
+    });
+
+    $('#item-section').css({
+        display:'none'
+    });
+
+    $('#order-section').css({
+        display:'none'
+    });
+});
+
+$('#nav-customer').on('click',() =>{
+    $('#dashboard-section').css({
+        display:'none'
+    });
+
+    $('#customer-section').css({
+        display:'block'
+    });
+
+    $('#item-section').css({
+        display:'none'
+    });
+
+    $('#order-section').css({
+        display:'none'
+    });
+});
+
+$('#nav-item').on('click',() =>{
+    $('#dashboard-section').css({
+        display:'none'
+    });
+
+    $('#customer-section').css({
+        display:'none'
+    });
+
+    $('#item-section').css({
+        display:'block'
+    });
+
+    $('#order-section').css({
+        display:'none'
+    });
+});
+
+$('#nav-order').on('click',() =>{
+    $('#dashboard-section').css({
+        display:'none'
+    });
+
+    $('#customer-section').css({
+        display:'none'
+    });
+
+    $('#item-section').css({
+        display:'none'
+    });
+
+    $('#order-section').css({
+        display:'block'
+    });
+});*/
+$('#dashboard-section').css({
+    display:'block'
+});
+
+$('#customer-section').css({
+    display:'none'
+});
+
+$('#item-section').css({
+    display:'none'
+});
+
+$('#order-section').css({
+    display:'none'
+});
+
+$('#nav-dashboard').on('click',() =>{
+    $('#dashboard-section').css({
+        display:'block'
+    });
+
+    $('#customer-section').css({
+        display:'none'
+    });
+
+    $('#item-section').css({
+        display:'none'
+    });
+
+    $('#order-section').css({
+        display:'none'
+    });
+});
+
+$('#nav-customer').on('click',() =>{
+    $('#dashboard-section').css({
+        display:'none'
+    });
+
+    $('#customer-section').css({
+        display:'block'
+    });
+
+    $('#item-section').css({
+        display:'none'
+    });
+
+    $('#order-section').css({
+        display:'none'
+    });
+});
+
+$('#nav-item').on('click',() =>{
+    $('#dashboard-section').css({
+        display:'none'
+    });
+
+    $('#customer-section').css({
+        display:'none'
+    });
+
+    $('#item-section').css({
+        display:'block'
+    });
+
+    $('#order-section').css({
+        display:'none'
+    });
+});
+
+$('#nav-order').on('click',() =>{
+    $('#dashboard-section').css({
+        display:'none'
+    });
+
+    $('#customer-section').css({
+        display:'none'
+    });
+
+    $('#item-section').css({
+        display:'none'
+    });
+
+    $('#order-section').css({
+        display:'block'
+    });
+});
